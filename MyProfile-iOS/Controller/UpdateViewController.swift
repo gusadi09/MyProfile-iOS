1d04479a825b58554ea5c5ef9bf62a637d3ca67f
gusadi09 <gusadi872@gmail.com>
2020-07-22 13:14:18 +0800

    make some preparation file MVC

---
+++ MyProfile-iOS/Controller/UpdateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UpdateViewController.swift
+//  MyProfile-iOS
+//
+//  Created by Gus Adi on 22/07/20.
+//  Copyright © 2020 Gus Adi. All rights reserved.
+//
+
+import UIKit
+
+class UpdateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyProfile-iOS/Util/RoundButton.swift
@@ -0,0 +1,9 @@
+//
+//  RoundButton.swift
+//  MyProfile-iOS
+//
+//  Created by Gus Adi on 22/07/20.
+//  Copyright © 2020 Gus Adi. All rights reserved.
+//
+
+import Foundation
